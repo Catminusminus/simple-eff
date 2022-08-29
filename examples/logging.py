a95832f2d886edda93e37a3bdcaca4989b015972
Catminusminus <getomya@svk.jp>
2022-08-30 02:54:22 +0900

    it works
    This is the first commit.

---
+++ examples/logging.py
@@ -0,0 +1,19 @@
+from simple_eff import Effect, Handler
+
+log = Effect()
+
+def plus_one(a: int):
+    yield log.perform(a)
+    return a + 1
+
+
+def handle_log(k, v):
+    print(f"Input value: {v}")
+    return k(None)
+
+
+if __name__ == '__main__':
+    handler = Handler()
+    handler.on(log, handle_log)
+    ret = handler.run(plus_one, 10)
+    print(ret)
---
+++ examples/logging_decorator.py
@@ -0,0 +1,20 @@
+from simple_eff import Effect, eff
+
+log = Effect()
+
+@eff
+def plus_one(a: int):
+    yield log.perform(a)
+    return a + 1
+
+
+def handle_log(k, v):
+    print(f"Input value: {v}")
+    return k(None)
+
+
+if __name__ == '__main__':
+    eleven = plus_one(10)
+    eleven.on(log, handle_log)
+    ret = eleven.run()
+    print(ret)
